db33f33122c1340aae151d49d781baff1e5da055
Maik Vinicius <maik_ga-eu@hotmail.com>
2021-08-02 04:41:16 -0300

    merge architecture

---
+++ app/src/main/java/com/pesquiseme/hotmart/domain/di/DomainModule.kt
@@ -0,0 +1,8 @@
+package com.pesquiseme.hotmart.domain.di
+
+import org.koin.dsl.module
+
+object DomainModule {
+    // factory
+    val domainModule = module {}
+}
---
+++ app/src/main/java/com/pesquiseme/hotmart/presentation/di/PresentationModule.kt
@@ -0,0 +1,9 @@
+package com.pesquiseme.hotmart.presentation.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+object PresentationModule {
+    // viewModel
+    val presentationModule = module {}
+}
